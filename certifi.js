e4efb1a7ff18318c221f6a56a6e87900602c9e98
reginavic <newmail.0518@gmail.com>
2024-02-08 10:08:16 +0300

    add certificates

--- certifi.js
+++ certifi.js
@@ -2,7 +2,7 @@
 
 const back = document.querySelector("#back");
 const next = document.querySelector("#next");
-const photos = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"];
+const photos = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpeg"];
 let i=0;
 next.addEventListener("click",() => {
     i++;
