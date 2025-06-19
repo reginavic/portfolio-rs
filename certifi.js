18c66a2c92125410223d301667f2619845d40bd0
reginavic <newmail.0518@gmail.com>
2025-06-19 07:22:34 +0300

    Portfolio update

--- certifi.js
+++ certifi.js
@@ -2,7 +2,7 @@
 
 const back = document.querySelector("#back");
 const next = document.querySelector("#next");
-const photos = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpeg"];
+const photos = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"];
 let i=0;
 next.addEventListener("click",() => {
     i++;
@@ -10,14 +10,14 @@ next.addEventListener("click",() => {
         i=0;
     }
     document.querySelector("#pictures").src=photos[i];
-})
+});
 back.addEventListener("click", () => {
     i--;
     if (i < 0) {
         i=photos.length-1;
     }
     document.querySelector("#pictures").src=photos[i];
-})
+});
 
 // Burger menu
 
@@ -27,9 +27,9 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+}));
--- index.js
+++ index.js
@@ -94,12 +94,12 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+}));
 
 // Animation for skills
 
